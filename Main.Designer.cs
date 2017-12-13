35f14713ae66fb53da7d64dcbb73f162591474ea
pdone <617941447@qq.com>
2017-12-13 19:41:33 +0800

    添加项目文件。

---
+++ Main.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Text2Voice
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Conversion = new System.Windows.Forms.Button();
+            this.OpenFolder = new System.Windows.Forms.Button();
+            this.tipInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Location = new System.Drawing.Point(8, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(373, 58);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Conversion
+            // 
+            this.Conversion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Conversion.Location = new System.Drawing.Point(131, 67);
+            this.Conversion.Name = "Conversion";
+            this.Conversion.Size = new System.Drawing.Size(117, 38);
+            this.Conversion.TabIndex = 1;
+            this.Conversion.Text = "Conversion";
+            this.Conversion.UseVisualStyleBackColor = true;
+            this.Conversion.Click += new System.EventHandler(this.Conversion_Click);
+            // 
+            // OpenFolder
+            // 
+            this.OpenFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OpenFolder.Location = new System.Drawing.Point(8, 67);
+            this.OpenFolder.Name = "OpenFolder";
+            this.OpenFolder.Size = new System.Drawing.Size(117, 38);
+            this.OpenFolder.TabIndex = 2;
+            this.OpenFolder.Text = "Open Folder";
+            this.OpenFolder.UseVisualStyleBackColor = true;
+            this.OpenFolder.Click += new System.EventHandler(this.OpenFolder_Click);
+            // 
+            // tipInfo
+            // 
+            this.tipInfo.AutoSize = true;
+            this.tipInfo.Location = new System.Drawing.Point(10, 120);
+            this.tipInfo.Name = "tipInfo";
+            this.tipInfo.Size = new System.Drawing.Size(47, 12);
+            this.tipInfo.TabIndex = 3;
+            this.tipInfo.Text = "Welcome";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 141);
+            this.Controls.Add(this.tipInfo);
+            this.Controls.Add(this.OpenFolder);
+            this.Controls.Add(this.Conversion);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Opacity = 0.8D;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text2Voice";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Conversion;
+        private System.Windows.Forms.Button OpenFolder;
+        private System.Windows.Forms.Label tipInfo;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Baidu.Aip.Speech;
+namespace Text2Voice
+{
+    public partial class Main : Form
+    {
+        string APP_ID = "10523072";
+        string API_KEY = "0Qr6nHXmGtCFD31ygXYWMAZG";
+        string SECRET_KEY = "3a68f4475b803651ac3517459f690093";
+        Color success = Color.SeaGreen;
+        Color error = Color.Crimson;
+        public Main()
+        {
+            InitializeComponent();
+            _asrClient = new Asr(API_KEY, SECRET_KEY);
+            _ttsClient = new Tts(API_KEY, SECRET_KEY);
+        }
+
+        private readonly Asr _asrClient;
+        private readonly Tts _ttsClient;
+
+        // 识别本地文件
+        public void AsrData()
+        {
+            var data = File.ReadAllBytes("语音pcm文件地址");
+            var result = _asrClient.Recognize(data, "pcm", 16000);
+            Console.Write(result);
+        }
+
+        // 识别URL中的语音文件
+        public void AsrUrl()
+        {
+            var result = _asrClient.Recoginze(
+                "http://xxx.com/待识别的pcm文件地址",
+                "http://xxx.com/识别结果回调地址",
+                "pcm",
+                16000);
+            Console.WriteLine(result);
+        }
+
+        // 合成
+        public void Tts(string text)
+        {
+            // 可选参数
+            var option = new Dictionary<string, object>()
+            {
+                {"spd", 4}, // 语速
+                {"vol", 7}, // 音量
+                {"per", 4}  // 发音人，4：情感度丫丫童声
+            };
+            var result = _ttsClient.Synthesis(text, option);
+
+            if (result.ErrorCode == 0)  // 或 result.Success
+            {
+                if (!Directory.Exists(@"E:\Text2Voice\"))
+                {
+                    Directory.CreateDirectory(@"E:\Text2Voice\");
+                }
+                string filePath = @"E:\Text2Voice\" + text + ".mp3";
+                File.WriteAllBytes(filePath, result.Data);
+                tipInfo.ForeColor = success;
+                tipInfo.Text = "Success";
+            }
+            else
+            {
+                tipInfo.ForeColor = error;
+                tipInfo.Text = "Error";
+            }
+        }
+
+        private void Conversion_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(richTextBox1.Text))
+                Tts(richTextBox1.Text.Trim());
+            else
+            {
+                tipInfo.ForeColor = error;
+                tipInfo.Text = "Please enter the correct content";
+            }
+        }
+
+        private void OpenFolder_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(@"E:\Text2Voice\"))
+            {
+                tipInfo.ForeColor = error;
+                tipInfo.Text = "Folder not exist";
+            }
+            else
+                System.Diagnostics.Process.Start(@"E:\Text2Voice");
+        }
+    }
+}
